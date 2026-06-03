8e58ef9b1ced91beeb2a2562917816a946a589ff
Sofie-Toll <sofie.bergqvist@toll.no>
2026-06-03 21:07:33 +0200

    cleanup

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,10 +1,15 @@
 using backend.Services;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors(options =>
